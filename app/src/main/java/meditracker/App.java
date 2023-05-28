880331abdcdcd03b2fcf8074cf5765e9f531a4d5
tobiokagae <jovapertama@gmail.com>
2023-05-29 04:06:25 +0800

    a

--- app/src/main/java/meditracker/App.java
+++ app/src/main/java/meditracker/App.java
@@ -9,74 +9,106 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
     private Stage stage;
     public void start(Stage primaryStage) throws Exception{
-        
         stage = primaryStage;  
-        SceneAwal();    
+        createScene1();    
         stage.show();
-
     }
 
-    private void SceneAwal(){
-        Label label = new Label("  Welcome To");
-        label.setStyle("-fx-font-family: 'Cambria'; -fx-font-size: 25px; -fx-text-color: #000000;");
-        Label label2 = new Label("MediTracker");
-        label2.setStyle("-fx-font-family: 'Cambria'; -fx-font-size: 35px; -fx-text-color: #000000;");
-
-        VBox vbox1 =new VBox(label,label2);
+    private void createScene1(){
+        Label label1 = new Label("MediTracker");
+        label1.setStyle("-fx-font-family: 'Cambria'; -fx-font-size: 35px; -fx-text-color: #000000; -fx-font-weight: bold;");
+        
+        ImageView logo = new ImageView(new Image("/meditrckr.png"));
+        logo.setFitHeight(150);
+        logo.setFitWidth(150);
+        
+        VBox vbox1 = new VBox(label1);
         vbox1.setAlignment(Pos.TOP_CENTER);
+
+        Button button1 = new Button("Sign In");
+        button1.setStyle("-fx-font-family: 'Cambria';-fx-padding: 10px 80px;-fx-text-fill:#FFFFFF;-fx-font-size: 15px;-fx-background-color: #C4DFDF; -fx-border-color: WHITE; -fx-border-width: 1px; -fx-border-radius: 2;");
+        button1.setOnAction(v -> {createScene2();});
+
+        Button button2 = new Button("EXIT");
+        button2.setStyle("-fx-font-family: 'Cambria';-fx-text-fill:RED;-fx-font-size: 15px;-fx-background-color: #FAEBD7; -fx-border-color: #FAEBD7; -fx-border-width: 1px; -fx-border-radius: 2;");
+        button2.setOnAction(v -> {createSceneExit();});
+
+        VBox vBox2 = new VBox(10,vbox1,logo, button1, button2);
+        vBox2.setAlignment(Pos.CENTER);
+        vBox2.setStyle("-fx-background-color:#F8F6F4;");
         
-        Label label3 = new Label("Login Member");
-        label3.setStyle("-fx-padding: 5px 25px;-fx-font-size: 30px; -fx-font-family: 'Times New Roman'; -fx-text-fill: BLACK;-fx-background-color: #FAEBD7; -fx-border-color: WHITE; -fx-border-width: 1px; -fx-border-radius: 5;");
-        Label label4= new Label();
-        VBox vbox2 = new VBox(10,label4,label3);
-        vbox2.setAlignment(Pos.TOP_CENTER);
-
-        TextField usernameField = new TextField();
-        usernameField.setStyle("-fx-padding: 1px 32px;-fx-text-fill:BLACK;-fx-font-size: 15px;-fx-background-color: #777777; -fx-border-color: #FAEBD7; -fx-border-width: 1px; -fx-border-radius: 3;");
-        usernameField.setPromptText("Username...");
-
-        TextField passwordField = new TextField();
-        passwordField.setStyle("-fx-padding: 1px 32px;-fx-text-fill:BLACK;-fx-font-size: 15px;-fx-background-color: #777777; -fx-border-color: #FAEBD7; -fx-border-width: 1px; -fx-border-radius: 3;");
-        passwordField.setPromptText("Password...");
-
-        HBox hbox1 = new HBox(usernameField);
-        hbox1.setAlignment(Pos.CENTER);
-        
-        HBox hbox2 = new HBox(passwordField);
-        hbox2.setAlignment(Pos.CENTER);
-
-        Button button1 = new Button("SIGN IN");
-        button1.setStyle("-fx-font-family: 'Cambria';-fx-padding: 10px 95px;-fx-text-fill:#FAEBD7;-fx-font-size: 15px;-fx-background-color: #006400; -fx-border-color: WHITE; -fx-border-width: 1px; -fx-border-radius: 2;");
-
-
-        Button button4 = new Button("Not Have Account?");
-        button4.setStyle("-fx-font-size: 10px; -fx-font-family: 'Times New Roman'; -fx-text-fill: BLACK;");
-
-        Button button2 = new Button("REGISTER");
-        button2.setStyle("-fx-font-family: 'Cambria';-fx-text-fill:BLACK;-fx-font-size: 15px;-fx-background-color: #FAEBD7; -fx-border-color: #FAEBD7; -fx-border-width: 1px; -fx-border-radius: 2;");
-
-        Button button3 = new Button("EXIT");
-        button3.setStyle("-fx-font-family: 'Cambria';-fx-text-fill:RED;-fx-font-size: 15px;-fx-background-color: #FAEBD7; -fx-border-color: #FAEBD7; -fx-border-width: 1px; -fx-border-radius: 2;");
-
-        HBox hbox3 = new HBox(5,button2,button3);
-        hbox3.setAlignment(Pos.CENTER);
-
-        VBox asli = new VBox(10,vbox1,vbox2,hbox1,hbox2,button1,button4,hbox3);
-        asli.setAlignment(Pos.CENTER);
-        asli.setStyle("-fx-background-color:#DCDCDC;");
-        // asli.getChildren().addAll(vbox1,vbox2,vbox3);
-        
-        Scene scene1 = new Scene(asli,620,620);
+        Scene scene1 = new Scene(vBox2,620,620);
         stage.setTitle("MediTracker");
         stage.setScene(scene1);
+    }
 
-        passwordField.setOnAction(event -> button1.fire());
-}
+    private void createSceneExit() {
+    }
+
+    private void createScene2() {
+        Label label = new Label("Log In Sebagai...");
+        label.setStyle("-fx-font-family: 'Cambria'; -fx-font-size: 25px; -fx-text-color: #000000; -fx-font-weight: bold;");
+
+        VBox vbox1 = new VBox(label);
+        vbox1.setAlignment(Pos.TOP_CENTER);
+
+        Button button1 = new Button("Dokter");
+        button1.setStyle("-fx-font-family: 'Cambria'; -fx-padding: 10px 80px;-fx-text-fill:#FFFFFF;-fx-font-size: 15px;-fx-background-color: #C4DFDF; -fx-border-color: WHITE; -fx-border-width: 1px; -fx-border-radius: 2;");
+        button1.setOnAction(v -> {createSceneLogInDokter();});
+
+        Button button2 = new Button("Pasien");
+        button2.setStyle("-fx-font-family: 'Cambria'; -fx-padding: 10px 80px;-fx-text-fill:#FFFFFF;-fx-font-size: 15px;-fx-background-color: #C4DFDF; -fx-border-color: WHITE; -fx-border-width: 1px; -fx-border-radius: 2;");
+        button2.setOnAction(v -> {createSceneLogInPasien();});
+
+        VBox vBox2 = new VBox(15, vbox1, button1, button2);
+        vBox2.setAlignment(Pos.CENTER);
+        vBox2.setStyle("-fx-background-color:#F8F6F4;");
+
+        Scene scene2 = new Scene(vBox2, 620, 620);
+        stage.setTitle("MediTracker");
+        stage.setScene(scene2);
+    }
+
+    private void createSceneLogInDokter() {
+    }
+
+    private void createSceneLogInPasien() {
+        Label label1 = new Label("Halo Dok! Silahkan Log In.");
+        label1.setStyle("-fx-font-family: 'Cambria'; -fx-font-size: 25px; -fx-text-color: #000000; -fx-font-weight: bold;");
+
+        TextField username = new TextField();
+        username.setStyle("-fx-padding: 1px 32px;-fx-text-fill:BLACK;-fx-font-size: 15px;-fx-font-color: #9DB2BF;-fx-background-color: #FFFAF4; -fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 3;");
+        username.setPromptText("Username...");
+
+        TextField id = new TextField();
+        id.setStyle("-fx-padding: 1px 32px;-fx-text-fill:BLACK;-fx-font-size: 15px;-fx-font-color: #9DB2BF;-fx-background-color: #FFFAF4; -fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 3;");
+        id.setPromptText("ID...");
+
+        HBox hBox1 = new HBox(label1);
+        hBox1.setAlignment(Pos.CENTER);
+
+        HBox hBox2 = new HBox(username);
+        hBox2.setAlignment(Pos.CENTER);
+
+        HBox hBox3 = new HBox(id);
+        hBox3.setAlignment(Pos.CENTER);
+
+        VBox vBox1 = new VBox(10,hBox1, hBox2, hBox3);
+        vBox1.setAlignment(Pos.CENTER);
+        vBox1.setStyle("-fx-background-color:#F8F6F4;");
+
+        Scene scene3 = new Scene(vBox1, 620, 620);
+        stage.setTitle("MediTracker");
+        stage.setScene(scene3);
+    }
 }
