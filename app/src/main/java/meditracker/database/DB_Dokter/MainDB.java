16f2758e3887f65201276c53f26bc4cfac262511
tobiokagae <jovapertama@gmail.com>
2023-05-30 12:05:18 +0800

    feat: membuat kerangka & database login dokter

---
+++ app/src/main/java/meditracker/database/DB_Dokter/MainDB.java
@@ -0,0 +1,11 @@
+package meditracker.database.DB_Dokter;
+
+public class MainDB {
+    public static void main(String[] args) {
+        // MyConfig.connection();
+        // MyConfig.AddData(71221010, "dr. Acli", 2093);
+        // MyConfig.AddData(71221101, "dr. L", 9083);
+        // MyConfig.AddData(71221065, "dr. Caca", 3495);
+        MyConfig.getDatabase();
+    }
+}
